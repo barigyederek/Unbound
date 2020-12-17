60fb0ac2f6f5ffe4a9135463fee6237095662dc9
barigyederek <derek.barigye@gmail.com>
2020-12-17 11:56:52 +0300

    Adding permission to read and write to storage.

--- app/src/main/java/com/example/unbound/MainActivity.java
+++ app/src/main/java/com/example/unbound/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.unbound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void toMainPage(View v){
+        Intent i  = new Intent(this, MainPage.class );
+        startActivity(i);
+    }
+
 }
